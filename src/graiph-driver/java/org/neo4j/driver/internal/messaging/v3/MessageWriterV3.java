9ed7ed697419f7f279788a878f2afcecbce24baf
bluejoe2008@gmail.com <bluejoe2008@gmail.com>
2019-11-07 16:13:55 +0800

    remove bolt protocol v5

--- src/graiph-driver/java/org/neo4j/driver/internal/messaging/v3/MessageWriterV3.java
+++ src/graiph-driver/java/org/neo4j/driver/internal/messaging/v3/MessageWriterV3.java
@@ -20,6 +20,8 @@ package org.neo4j.driver.internal.messaging.v3;
 
 import java.util.Map;
 
+import cn.graiph.blob.BlobMessageSignature;
+import org.neo4j.driver.internal.GetBlobMessageEncoder;
 import org.neo4j.driver.internal.messaging.AbstractMessageWriter;
 import org.neo4j.driver.internal.messaging.MessageEncoder;
 import org.neo4j.driver.internal.messaging.encode.BeginMessageEncoder;
@@ -56,6 +58,10 @@ public class MessageWriterV3 extends AbstractMessageWriter
         Map<Byte,MessageEncoder> result = Iterables.newHashMapWithSize( 9 );
         result.put( HelloMessage.SIGNATURE, new HelloMessageEncoder() );
         result.put( GoodbyeMessage.SIGNATURE, new GoodbyeMessageEncoder() );
+
+        //NOTE: GetBlobMessageEncoder
+        result.put(BlobMessageSignature.SIGNATURE_GET_BLOB(), new GetBlobMessageEncoder() ); // new
+        //NOTE
 
         result.put( RunWithMetadataMessage.SIGNATURE, new RunWithMetadataMessageEncoder() );
         result.put( DiscardAllMessage.SIGNATURE, new DiscardAllMessageEncoder() );
