c62f2b3d88f3a21392b807673a62493890f40509
bluejoe2008@gmail.com <bluejoe2008@gmail.com>
2019-11-07 18:09:57 +0800

    database lifecycle plugins

---
+++ src/externel-properties/scala/cn/graiph/GNodeServerPlugin.scala
@@ -0,0 +1,37 @@
+package cn.graiph
+
+import org.neo4j.kernel.impl.blob.{CustomDatabaseLifecyclePluginContext, CustomDatabaseLifecyclePlugin}
+
+/**
+  * Created by bluejoe on 2019/11/7.
+  */
+class GNodeServerPlugin extends CustomDatabaseLifecyclePlugin{
+  override def init(ctx: CustomDatabaseLifecyclePluginContext): Unit = {
+    val zk = ctx.configuration.getRaw("...");
+    //...
+    ctx.instanceContext.put("", null);
+    //ctx.instanceContext.put[CustomPropertyNodeStoreHolder]();
+  }
+
+  override def stop(ctx: CustomDatabaseLifecyclePluginContext): Unit = {
+
+  }
+
+  override def start(ctx: CustomDatabaseLifecyclePluginContext): Unit = {
+
+  }
+}
+
+class CNodeServerPlugin extends CustomDatabaseLifecyclePlugin{
+  override def init(ctx: CustomDatabaseLifecyclePluginContext): Unit = {
+
+  }
+
+  override def stop(ctx: CustomDatabaseLifecyclePluginContext): Unit = {
+
+  }
+
+  override def start(ctx: CustomDatabaseLifecyclePluginContext): Unit = {
+
+  }
+}
