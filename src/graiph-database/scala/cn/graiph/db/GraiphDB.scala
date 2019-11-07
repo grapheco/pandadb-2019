f9f49ea516712ce23f75a7b84fdb7c8c8122c84c
bluejoe2008@gmail.com <bluejoe2008@gmail.com>
2019-11-07 17:46:50 +0800

    database lifecycle plugins

--- src/graiph-database/scala/cn/graiph/db/GraiphDB.scala
+++ src/graiph-database/scala/cn/graiph/db/GraiphDB.scala
@@ -7,14 +7,14 @@ import cn.graiph.util.Logging
 import cn.graiph.{CustomPropertyProvider, CypherPluginRegistry, ValueMatcher}
 import org.neo4j.graphdb.GraphDatabaseService
 import org.neo4j.graphdb.factory.GraphDatabaseFactory
-import org.neo4j.kernel.impl.blob.{CustomDatabaseLifecyclePluginContext, CustomDatabaseLifecyclePlugin, CustomDatabaseLifecyclePlugins}
+import org.neo4j.kernel.impl.blob.{CustomDatabaseLifecyclePlugin, CustomDatabaseLifecyclePluginContext, CustomDatabaseLifecyclePlugins}
 import org.springframework.context.support.FileSystemXmlApplicationContext
 
 /**
   * Created by bluejoe on 2019/7/17.
   */
 object GraiphDB extends Logging with Touchable {
-  SemanticOperatorPluginInjection.touch;
+  CustomDatabaseLifecyclePlugins.register[SemanticOperatorPlugin];
 
   def openDatabase(dbDir: File, propertiesFile: File): GraphDatabaseService = {
     val builder = new GraphDatabaseFactory().newEmbeddedDatabaseBuilder(dbDir);
@@ -31,55 +31,51 @@ object GraiphDB extends Logging with Touchable {
 }
 
 trait Touchable {
-  //do nothing, just ensure this object is initialized
-  final def touch: Unit = {
+  def touch(): Unit = {
+    //do nothing, activate this class static {}
   }
 }
 
-object SemanticOperatorPluginInjection extends Touchable {
-  CustomDatabaseLifecyclePlugins.register(new SemanticOperatorPlugin());
+class SemanticOperatorPlugin extends CustomDatabaseLifecyclePlugin with Logging {
+  override def init(ctx: CustomDatabaseLifecyclePluginContext): Unit = {
+    val configuration = ctx.configuration;
+    val cypherPluginRegistry = configuration.getRaw("blob.plugins.conf").map(x => {
+      val xml = new File(x);
 
-  class SemanticOperatorPlugin extends CustomDatabaseLifecyclePlugin with Logging {
-    override def init(ctx: CustomDatabaseLifecyclePluginContext): Unit = {
-      val configuration = ctx.configuration;
-      val cypherPluginRegistry = configuration.getRaw("blob.plugins.conf").map(x => {
-        val xml = new File(x);
-
-        val path =
-          if (xml.isAbsolute) {
-            xml.getPath
+      val path =
+        if (xml.isAbsolute) {
+          xml.getPath
+        }
+        else {
+          val configFilePath = configuration.getRaw("config.file.path")
+          if (configFilePath.isDefined) {
+            new File(new File(configFilePath.get).getParentFile, x).getAbsoluteFile.getCanonicalPath
           }
           else {
-            val configFilePath = configuration.getRaw("config.file.path")
-            if (configFilePath.isDefined) {
-              new File(new File(configFilePath.get).getParentFile, x).getAbsoluteFile.getCanonicalPath
-            }
-            else {
-              xml.getAbsoluteFile.getCanonicalPath
-            }
+            xml.getAbsoluteFile.getCanonicalPath
           }
+        }
 
-        logger.info(s"loading semantic plugins: $path");
-        val appctx = new FileSystemXmlApplicationContext("file:" + path);
-        appctx.getBean[CypherPluginRegistry](classOf[CypherPluginRegistry]);
-      }).getOrElse {
-        logger.info(s"semantic plugins not loaded: blob.plugins.conf=null");
-        new CypherPluginRegistry()
-      }
-
-      val customPropertyProvider = cypherPluginRegistry.createCustomPropertyProvider(configuration);
-      val valueMatcher = cypherPluginRegistry.createValueComparatorRegistry(configuration);
-
-      ctx.instanceContext.put[CustomPropertyProvider](customPropertyProvider);
-      ctx.instanceContext.put[ValueMatcher](valueMatcher);
+      logger.info(s"loading semantic plugins: $path");
+      val appctx = new FileSystemXmlApplicationContext("file:" + path);
+      appctx.getBean[CypherPluginRegistry](classOf[CypherPluginRegistry]);
+    }).getOrElse {
+      logger.info(s"semantic plugins not loaded: blob.plugins.conf=null");
+      new CypherPluginRegistry()
     }
 
-    override def stop(ctx: CustomDatabaseLifecyclePluginContext): Unit = {
+    val customPropertyProvider = cypherPluginRegistry.createCustomPropertyProvider(configuration);
+    val valueMatcher = cypherPluginRegistry.createValueComparatorRegistry(configuration);
 
-    }
+    ctx.instanceContext.put[CustomPropertyProvider](customPropertyProvider);
+    ctx.instanceContext.put[ValueMatcher](valueMatcher);
+  }
 
-    override def start(ctx: CustomDatabaseLifecyclePluginContext): Unit = {
+  override def stop(ctx: CustomDatabaseLifecyclePluginContext): Unit = {
 
-    }
+  }
+
+  override def start(ctx: CustomDatabaseLifecyclePluginContext): Unit = {
+
   }
 }
