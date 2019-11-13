0afa4e88aeba03b9aabc15801a6b48e9dda2e7ff
bluejoe2008@gmail.com <bluejoe2008@gmail.com>
2019-11-13 12:16:07 +0800

    java driver

--- graiph-java-driver/src/main/java/org/neo4j/driver/internal/messaging/v3/MessageWriterV3.java
+++ graiph-java-driver/src/main/java/org/neo4j/driver/internal/messaging/v3/MessageWriterV3.java
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.neo4j.driver.internal.messaging.v4;
+package org.neo4j.driver.internal.messaging.v3;
 
 import java.util.Map;
 
@@ -26,19 +26,19 @@ import org.neo4j.driver.internal.messaging.AbstractMessageWriter;
 import org.neo4j.driver.internal.messaging.MessageEncoder;
 import org.neo4j.driver.internal.messaging.encode.BeginMessageEncoder;
 import org.neo4j.driver.internal.messaging.encode.CommitMessageEncoder;
-import org.neo4j.driver.internal.messaging.encode.DiscardMessageEncoder;
+import org.neo4j.driver.internal.messaging.encode.DiscardAllMessageEncoder;
 import org.neo4j.driver.internal.messaging.encode.GoodbyeMessageEncoder;
 import org.neo4j.driver.internal.messaging.encode.HelloMessageEncoder;
-import org.neo4j.driver.internal.messaging.encode.PullMessageEncoder;
+import org.neo4j.driver.internal.messaging.encode.PullAllMessageEncoder;
 import org.neo4j.driver.internal.messaging.encode.ResetMessageEncoder;
 import org.neo4j.driver.internal.messaging.encode.RollbackMessageEncoder;
 import org.neo4j.driver.internal.messaging.encode.RunWithMetadataMessageEncoder;
 import org.neo4j.driver.internal.messaging.request.BeginMessage;
 import org.neo4j.driver.internal.messaging.request.CommitMessage;
-import org.neo4j.driver.internal.messaging.request.DiscardMessage;
+import org.neo4j.driver.internal.messaging.request.DiscardAllMessage;
 import org.neo4j.driver.internal.messaging.request.GoodbyeMessage;
 import org.neo4j.driver.internal.messaging.request.HelloMessage;
-import org.neo4j.driver.internal.messaging.request.PullMessage;
+import org.neo4j.driver.internal.messaging.request.PullAllMessage;
 import org.neo4j.driver.internal.messaging.request.ResetMessage;
 import org.neo4j.driver.internal.messaging.request.RollbackMessage;
 import org.neo4j.driver.internal.messaging.request.RunWithMetadataMessage;
@@ -46,9 +46,9 @@ import org.neo4j.driver.internal.messaging.v2.ValuePackerV2;
 import org.neo4j.driver.internal.packstream.PackOutput;
 import org.neo4j.driver.internal.util.Iterables;
 
-public class MessageWriterV4 extends AbstractMessageWriter
+public class MessageWriterV3 extends AbstractMessageWriter
 {
-    public MessageWriterV4( PackOutput output )
+    public MessageWriterV3( PackOutput output )
     {
         super( new ValuePackerV2( output ), buildEncoders() );
     }
@@ -58,15 +58,18 @@ public class MessageWriterV4 extends AbstractMessageWriter
         Map<Byte,MessageEncoder> result = Iterables.newHashMapWithSize( 9 );
         result.put( HelloMessage.SIGNATURE, new HelloMessageEncoder() );
         result.put( GoodbyeMessage.SIGNATURE, new GoodbyeMessageEncoder() );
-        result.put( RunWithMetadataMessage.SIGNATURE, new RunWithMetadataMessageEncoder() );
 
-        result.put( DiscardMessage.SIGNATURE, new DiscardMessageEncoder() ); // new
-        result.put( PullMessage.SIGNATURE, new PullMessageEncoder() ); // new
+        result.put( RunWithMetadataMessage.SIGNATURE, new RunWithMetadataMessageEncoder() );
+        result.put( DiscardAllMessage.SIGNATURE, new DiscardAllMessageEncoder() );
+        result.put( PullAllMessage.SIGNATURE, new PullAllMessageEncoder() );
+
+        //NOTE: GetBlobMessageEncoder
+        result.put(BlobMessageSignature.SIGNATURE_GET_BLOB(), new GetBlobMessageEncoder() ); // new
+        //NOTE
 
         result.put( BeginMessage.SIGNATURE, new BeginMessageEncoder() );
         result.put( CommitMessage.SIGNATURE, new CommitMessageEncoder() );
         result.put( RollbackMessage.SIGNATURE, new RollbackMessageEncoder() );
-
         result.put( ResetMessage.SIGNATURE, new ResetMessageEncoder() );
         return result;
     }
