c2f7d691f66ac2a4a19318056f6a20a0d92d7246
Airzihao <airzihao@163.com>
2019-11-15 09:52:31 +0800

    Add distribution test design(only a draft).

---
+++ itest/src/test/scala/Distribution/WriteTest.scala
@@ -0,0 +1,25 @@
+package Distribution
+
+import org.junit.Test
+
+class WriteTest {
+
+
+  @Test
+  def testCreate(): Unit ={
+
+  }
+
+  @Test
+  def testMerge(): Unit ={
+
+  }
+
+
+  // May not work. Involved in data dispatch problem.
+  @Test
+  def testLoadCSV(): Unit ={
+
+  }
+
+}
