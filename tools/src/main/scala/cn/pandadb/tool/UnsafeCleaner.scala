5e9cf901ae32307722793326ba7c313cc970bfa6
bluejoe2008@gmail.com <bluejoe2008@gmail.com>
2019-11-21 21:37:16 +0800

    add module tools

---
+++ tools/src/main/scala/cn/pandadb/tool/UnsafeCleaner.scala
@@ -0,0 +1,10 @@
+package cn.pandadb.tool
+
+/**
+  * Created by bluejoe on 2019/11/21.
+  */
+object UnsafeCleaner {
+  def clean(zkHosts: String): Unit = {
+
+  }
+}
---
+++ tools/src/main/scala/cn/pandadb/tool/UnsafeImporter.scala
@@ -0,0 +1,13 @@
+package cn.pandadb.tool
+
+import java.io.File
+
+/**
+  * Created by bluejoe on 2019/11/21.
+  */
+object UnsafeImporter {
+
+  def importData(zkHosts: String, csv: File): Unit = {
+
+  }
+}
