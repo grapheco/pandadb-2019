16455a2e34be9af148a120ebf34340fd020c370a
bluejoe2008@gmail.com <bluejoe2008@gmail.com>
2019-11-22 15:55:52 +0800

    cluster manager

---
+++ network-commons/src/main/scala/cn/pandadb/network/events.scala
@@ -0,0 +1,41 @@
+package cn.pandadb.network
+
+trait ClusterEvent {
+
+}
+
+case class ClusterStateChanged() extends ClusterEvent {
+
+}
+
+case class NodeConnected() extends ClusterEvent {
+
+}
+
+case class NodeDisconnected() extends ClusterEvent {
+
+}
+
+case class ReadRequestAccepted() extends ClusterEvent {
+
+}
+
+case class WriteRequestAccepted() extends ClusterEvent {
+
+}
+
+case class ReadRequestCompleted() extends ClusterEvent {
+
+}
+
+case class WriteRequestCompleted() extends ClusterEvent {
+
+}
+
+case class MasterWriteNodeSeleted() extends ClusterEvent {
+
+}
+
+case class NodeDetail(address: NodeAddress, writable: Boolean, readable: Boolean) {
+
+}
