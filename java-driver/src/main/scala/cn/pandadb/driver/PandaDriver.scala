1f9a7d372f7a3ce35a8930b52a080bfbc550ac2d
codeBabyLin <1163718576@qq.com>
2019-11-25 14:56:21 +0800

    wipe red waves and add node-pick up function Signed-off-by: codeBabyLin <1163718576@qq.com>

--- java-driver/src/main/scala/cn/pandadb/driver/PandaDriver.scala
+++ java-driver/src/main/scala/cn/pandadb/driver/PandaDriver.scala
@@ -6,7 +6,7 @@ import java.security.GeneralSecurityException
 import java.{security, util}
 import java.util.concurrent.{CompletableFuture, CompletionStage}
 
-import cn.pandadb.network.{ClusterClient}
+import cn.pandadb.network.{ClusterClient, NodeAddress}
 import org.neo4j.driver.Config.TrustStrategy
 import org.neo4j.driver._
 import org.neo4j.driver.async.{AsyncSession, AsyncStatementRunner, AsyncTransaction, AsyncTransactionWork, StatementResultCursor}
@@ -51,16 +51,22 @@ class PandaDriver(uri: String, authToken: AuthToken, config: Config) extends Dri
   //override def defaultTypeSystem(): TypeSystem = ???
   override def defaultTypeSystem(): TypeSystem = InternalTypeSystem.TYPE_SYSTEM
 
-  override def rxSession(): RxSession = ???
+  //override def rxSession(): RxSession = ???
+  override def rxSession(): RxSession = null
 
 
-  override def rxSession(sessionConfig: SessionConfig): RxSession = ???
+
+  //override def rxSession(sessionConfig: SessionConfig): RxSession = ???
+  override def rxSession(sessionConfig: SessionConfig): RxSession = null
+
 
   /**
    *   verifyConnectivityAsync and verifyConnectivity  is not right ,  because uri is zkString
    */
 
-  override def verifyConnectivityAsync(): CompletionStage[Void] = ???
+  //override def verifyConnectivityAsync(): CompletionStage[Void] = ???
+  override def verifyConnectivityAsync(): CompletionStage[Void] = null
+
 
 
   //override def verifyConnectivity(): Unit = ???
@@ -73,25 +79,30 @@ class PandaDriver(uri: String, authToken: AuthToken, config: Config) extends Dri
   override def metrics(): Metrics = {
     createDriverMetrics(config, this.createClock()).metrics()
   }
-  def createDriverMetrics(config:Config , clock : Clock ):MetricsProvider= {
+  private def createDriverMetrics(config: Config, clock: Clock ): MetricsProvider = {
     if (config.isMetricsEnabled()) new InternalMetricsProvider(clock) else MetricsProvider.METRICS_DISABLED_PROVIDER
   }
-  def createClock():Clock = {
+  private def createClock(): Clock = {
     Clock.SYSTEM
   }
 
 
 
+  //override def asyncSession(): AsyncSession = ???
+  override def asyncSession(): AsyncSession = null
 
-  override def asyncSession(): AsyncSession = ???
+  //override def asyncSession(sessionConfig: SessionConfig): AsyncSession = ???
+  override def asyncSession(sessionConfig: SessionConfig): AsyncSession = null
 
-  override def asyncSession(sessionConfig: SessionConfig): AsyncSession = ???
 
   override def close(): Unit = {
 
   }
 
-  override def isEncrypted: Boolean = ???
+
+  //wait to finish
+  //override def isEncrypted: Boolean = ???
+  override def isEncrypted: Boolean = true
 
 
 
@@ -103,13 +114,43 @@ class PandaDriver(uri: String, authToken: AuthToken, config: Config) extends Dri
 
 class PandaSession(sessionConfig: SessionConfig, clusterOperator: ClusterClient) extends Session {
 
-  val host = "10.0.86.179"
+  var host = "10.0.86.179"
 
-  val port = 7687
-  val uri = s"bolt://$host:$port"
-  val driver = GraphDatabase.driver(uri, AuthTokens.basic("neo4j", "neo4j"))
-  val session = driver.session(sessionConfig)
+  var port = 7687
+  var uri = s"bolt://$host:$port"
+  var driver = GraphDatabase.driver(uri, AuthTokens.basic("neo4j", "123456"))
+  //var driver = GraphDatabase.driver(uri)
+  var session = driver.session(sessionConfig)
 
+  private def isRead(statement: String): Boolean = {
+    val tempStatement = statement.toLowerCase()
+    if (tempStatement.contains("create") || tempStatement.contains("merge") ||
+      tempStatement.contains("set") || tempStatement.contains("delete")) {
+      false
+    }
+    else true
+  }
+  private def getWriteNode(): NodeAddress = {
+    clusterOperator.getWriteMasterNode()
+  }
+  private def getReadNode(): NodeAddress = {
+    //random to pick up a node
+    val nodeLists = clusterOperator.getAllNodes().toList
+    val index = (new util.Random).nextInt(nodeLists.length)
+    nodeLists(index)
+  }
+  private def getNode(statement: String): NodeAddress = {
+    if (isRead(statement)) getReadNode() else getWriteNode()
+  }
+  private def getSessionReady(statement: String): Unit = {
+    val node = getNode(statement)
+    this.host = node.host
+    this.port = node.port
+    this.uri = s"bolt://$host:$port"
+    this.driver = GraphDatabase.driver(uri, AuthTokens.basic("neo4j", "neo4j"))
+    this.session = driver.session(sessionConfig)
+
+  }
   //override def writeTransaction[T](work: TransactionWork[T]): T = ???
   override def writeTransaction[T](work: TransactionWork[T]): T = {
     session.writeTransaction(work)
@@ -132,16 +173,16 @@ class PandaSession(sessionConfig: SessionConfig, clusterOperator: ClusterClient)
 
   //override def run(statement: String, config: TransactionConfig): StatementResult = ???
   override def run(statement: String, config: TransactionConfig): StatementResult = {
-    session.run(statement,config)
+    session.run(statement, config)
   }
 
   //override def run(statement: String, parameters: util.Map[String, AnyRef], config: TransactionConfig): StatementResult = ???
   override def run(statement: String, parameters: util.Map[String, AnyRef], config: TransactionConfig): StatementResult = {
-    session.run(statement,parameters,config)
+    session.run(statement, parameters, config)
   }
   //override def run(statement: Statement, config: TransactionConfig): StatementResult = ???
   override def run(statement: Statement, config: TransactionConfig): StatementResult = {
-    session.run(statement,config)
+    session.run(statement, config)
   }
 
   //override def close(): Unit = ???
@@ -171,18 +212,18 @@ class PandaSession(sessionConfig: SessionConfig, clusterOperator: ClusterClient)
 
  // override def run(statementTemplate: String, parameters: Value): StatementResult = ???
  override def run(statementTemplate: String, parameters: Value): StatementResult = {
-   session.run(statementTemplate,parameters)
+   session.run(statementTemplate, parameters)
  }
 
 
   //override def run(statementTemplate: String, statementParameters: util.Map[String, AnyRef]): StatementResult = ???
   override def run(statementTemplate: String, statementParameters: util.Map[String, AnyRef]): StatementResult = {
-    session.run(statementTemplate,statementParameters)
+    session.run(statementTemplate, statementParameters)
   }
 
   //override def run(statementTemplate: String, statementParameters: Record): StatementResult = ???
   override def run(statementTemplate: String, statementParameters: Record): StatementResult = {
-    session.run(statementTemplate,statementParameters)
+    session.run(statementTemplate, statementParameters)
   }
 
   //override def run(statementTemplate: String): StatementResult = ???
