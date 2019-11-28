7b17e89b57204ffb77b6c92347c3ddce02a385d8
codeBabyLin <1163718576@qq.com>
2019-11-28 14:50:19 +0800

    policy to pick up node

--- java-driver/src/main/scala/cn/pandadb/driver/SelectNode.scala
+++ java-driver/src/main/scala/cn/pandadb/driver/SelectNode.scala
@@ -9,34 +9,53 @@ import org.neo4j.driver.{AuthTokens, Driver, GraphDatabase}
  * @Date: Created at 20:50 2019/11/27
  * @Modified By:
  */
+trait Strategy{
 
+}
+case class RANDOM_PICK() extends Strategy{
+
+}
+case class WORK_TIME_PICK() extends Strategy{
+
+}
+case class DEFAULT_PICK() extends Strategy{
+
+}
 object SelectNode {
 
+  val RONDOM_POLICY = 0
+  val _POLICY = 1
 
   private def getWriteNode(clusterOperator: ClusterClient): NodeAddress = {
-    //isReadNode = false
     //clusterOperator.getWriteMasterNode()
-    val hos = "10.0.86.179"
-    val por = 7687
-    new NodeAddress(hos, por)
+    policyDefault
   }
-  private def getReadNode(clusterOperator: ClusterClient): NodeAddress = {
-    //random to pick up a node
 
-    //isReadNode = true
-    //val nodeLists = clusterOperator.getAllNodes().toList
-    //val index = (new util.Random).nextInt(nodeLists.length)
-    // nodeLists(index)
+  private def policyRandom(clusterOperator: ClusterClient): NodeAddress = {
+
+    val nodeLists = clusterOperator.getAllNodes().toList
+    val index = (new util.Random).nextInt(nodeLists.length)
+    nodeLists(index)
+
+  }
+  private def policyDefault(): NodeAddress = {
     val hos = "10.0.86.179"
     val por = 7687
     new NodeAddress(hos, por)
   }
-  private def getNode(isWriteStatement: Boolean, clusterOperator: ClusterClient): NodeAddress = {
-    if (isWriteStatement) getWriteNode(clusterOperator) else getReadNode(clusterOperator)
+  private def getReadNode(clusterOperator: ClusterClient, strategy: Strategy): NodeAddress = {
+    strategy match {
+      case RANDOM_PICK() => policyRandom(clusterOperator)
+      case DEFAULT_PICK() => policyDefault
+      case _ => policyDefault;
+    }
+  }
+  private def getNode(isWriteStatement: Boolean, clusterOperator: ClusterClient, strategy: Strategy): NodeAddress = {
+    if (isWriteStatement) getWriteNode(clusterOperator) else getReadNode(clusterOperator, strategy)
   }
 
   def getDriver(isWriteStatement: Boolean, clusterOperator: ClusterClient): Driver = {
-    val node = getNode(isWriteStatement, clusterOperator)
+    val node = getNode(isWriteStatement, clusterOperator, new DEFAULT_PICK)
     val host = node.host
     val port = node.port
     val uri = s"bolt://$host:$port"
