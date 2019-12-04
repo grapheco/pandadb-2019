035608dc91281db976a4594bf01b7108be8820fe
Airzihao <airzihao@163.com>
2019-12-04 14:46:10 +0800

    Add GlobalContext.

---
+++ network-commons/src/main/scala/cn/pandadb/server/GlobalContext.scala
@@ -0,0 +1,21 @@
+package cn.pandadb.server
+
+import java.io.File
+
+import cn.pandadb.network.ClusterClient
+import cn.pandadb.util.ContextMap
+
+/**
+  * @Author: Airzihao
+  * @Description:
+  * @Date: Created at 14:42 2019/12/4
+  * @Modified By:
+  */
+object GlobalContext extends ContextMap {
+
+  def putStoreDir(storeDir: File): Unit = {
+    this.put[File]("pnode.store.dir", storeDir)
+  }
+
+  def getStoreDir: File = this.get[File]("pnode.store.dir")
+}
