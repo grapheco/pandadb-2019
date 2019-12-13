8429a6ce4cb15fbcd498de5eb4bc4e88a6d8be38
excelwang <wanghuajin999@163.com>
2019-12-13 14:29:12 +0800

    add testJoin

--- itest/src/test/scala/external-properties/ppd/InMemoryPredicatePushDown.scala
+++ itest/src/test/scala/external-properties/ppd/InMemoryPredicatePushDown.scala
@@ -1,4 +1,4 @@
-package InMemoryPredicatePushDown
+package ppd
 
 import java.io.File
 import cn.pandadb.server.PNodeServer
@@ -9,8 +9,7 @@ import org.neo4j.io.fs.FileUtils
 import cn.pandadb.externalprops.{CustomPropertyNodeStore, InMemoryPropertyNodeStore, InMemoryPropertyNodeStoreFactory}
 import cn.pandadb.server.GlobalContext
 
-trait CreateQueryTestBase {
-  var db: GraphDatabaseService = null
+class InMemoryPredicatePushDown extends QueryCase {
 
   @Before
   def initdb(): Unit = {
@@ -21,57 +20,6 @@ trait CreateQueryTestBase {
       setConfig("external.properties.store.factory", classOf[InMemoryPropertyNodeStoreFactory].getName).
       newGraphDatabase()
     GlobalContext.put(classOf[CustomPropertyNodeStore].getName, InMemoryPropertyNodeStore)
-
-  }
-
-  @After
-  def shutdowndb(): Unit = {
-    db.shutdown()
-  }
-
-  protected def testQuery[T](query: String): Unit = {
-    val tx = db.beginTx();
-    val rs = db.execute(query);
-    while (rs.hasNext) {
-      val row = rs.next();
-    }
-    tx.success();
-    tx.close()
-  }
-}
-
-class PredicatePushDown extends CreateQueryTestBase {
-  val tmpns = InMemoryPropertyNodeStore
-
-  @Test
-  def testLessThan(): Unit = {
-    // create one node
-    val query = "CREATE (n:Person {age: 10, name: 'bob'})"
-    db.execute(query)
-    val query2 = "match (n) where 18>n.age return id(n)"
-    val rs2 = db.execute(query2)
-    var id2: Long = -1
-    if (rs2.hasNext) {
-      val row = rs2.next()
-      id2 = row.get("id(n)").toString.toLong
-    }
-    assert(id2 != -1)
-  }
-
-  @Test
-  def testGreaterThan(): Unit = {
-    // create one node
-    val query = "CREATE (n:Person {age: 10, name: 'bob'})"
-    db.execute(query)
-
-    val query3 = "match (n) where 18<n.age return id(n)"
-    val rs3 = db.execute(query3)
-    var id3: Long = -1
-    if (rs3.hasNext) {
-      val row = rs3.next()
-      id3 = row.get("id(n)").toString.toLong
-    }
-    assert(id3 == -1)
   }
 
 }
--- itest/src/test/scala/external-properties/ppd/InSolrPredicatePushDown.scala
+++ itest/src/test/scala/external-properties/ppd/InSolrPredicatePushDown.scala
@@ -1,4 +1,4 @@
-package InSolrPredicatePushDown
+package ppd
 
 import java.io.{File, FileInputStream}
 import java.util.Properties
@@ -8,11 +8,10 @@ import org.junit.{After, Assert, Before, Test}
 import org.neo4j.graphdb.factory.GraphDatabaseFactory
 import org.neo4j.graphdb.{GraphDatabaseService, Result}
 import org.neo4j.io.fs.FileUtils
-import cn.pandadb.externalprops.{CustomPropertyNodeStore, InSolrPropertyNodeStore, InSolrPropertyNodeStoreFactory}
+import cn.pandadb.externalprops.{CustomPropertyNodeStore, InMemoryPropertyNodeStore, InMemoryPropertyNodeStoreFactory, InSolrPropertyNodeStore, InSolrPropertyNodeStoreFactory}
 import cn.pandadb.server.GlobalContext
 
-trait CreateQueryTestBase1 {
-  var db: GraphDatabaseService = null
+class InSolrPredicatePushDown extends QueryCase {
 
   @Before
   def initdb(): Unit = {
@@ -31,71 +30,6 @@ trait CreateQueryTestBase1 {
     solrNodeStore.clearAll()
     GlobalContext.put(classOf[CustomPropertyNodeStore].getName, solrNodeStore)
 
-  }
-
-  @After
-  def shutdowndb(): Unit = {
-    db.shutdown()
-  }
-
-  protected def testQuery[T](query: String): Unit = {
-    val tx = db.beginTx();
-    val rs = db.execute(query);
-    while (rs.hasNext) {
-      val row = rs.next();
-    }
-    tx.success();
-    tx.close()
-  }
-}
-
-class PredicatePushDown extends CreateQueryTestBase1 {
-
-  @Test
-  def testEqual(): Unit = {
-    // create one node
-    val query = "CREATE (n:Person {age: 10, name: 'bob'})"
-    db.execute(query)
-    val query2 = "match (n) where n.name = 'bob' return id(n)"
-    val rs2 = db.execute(query2)
-    var id2: Long = -1
-    if (rs2.hasNext) {
-      val row = rs2.next()
-      id2 = row.get("id(n)").toString.toLong
-    }
-    assert(id2 != -1)
-  }
-
-  @Test
-  def testEndsWith(): Unit = {
-    // create one node
-    val query = "CREATE (n:Person {age: 10, name: 'bob'})"
-    db.execute(query)
-
-    val query3 = "match (n) where n.name ENDS WITH 'a' return id(n)"
-    val rs3 = db.execute(query3)
-    var id3: Long = -1
-    if (rs3.hasNext) {
-      val row = rs3.next()
-      id3 = row.get("id(n)").toString.toLong
-    }
-    assert(id3 == -1)
-  }
-
-  @Test
-  def testLableAndEndsWith(): Unit = {
-    // create one node
-    val query = "CREATE (n:Person {age: 10, name: 'bob'})"
-    db.execute(query)
-
-    val query3 = "match (n:Person) where n.name ENDS WITH 'a' return id(n)"
-    val rs3 = db.execute(query3)
-    var id3: Long = -1
-    if (rs3.hasNext) {
-      val row = rs3.next()
-      id3 = row.get("id(n)").toString.toLong
-    }
-    assert(id3 == -1)
   }
 
 }
