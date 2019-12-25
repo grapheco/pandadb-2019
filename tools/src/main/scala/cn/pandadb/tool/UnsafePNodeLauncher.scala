32120c6e8f9e2434939b7b736a1765f7071f9fab
Airzihao <airzihao@163.com>
2019-12-25 23:36:48 +0800

    PandaDBTestBase, support starting PNondeServer.

---
+++ tools/src/main/scala/cn/pandadb/tool/UnsafePNodeLauncher.scala
@@ -0,0 +1,18 @@
+package cn.pandadb.tool
+
+/**
+  * @Author: Airzihao
+  * @Description:
+  * @Date: Created in 22:27 2019/12/25
+  * @Modified By:
+  */
+object UnsafePNodeLauncher {
+  def main(args: Array[String]): Unit = {
+    val num = args(0)
+    PNodeServerStarter.main(Array(s"../itest/output/testdb/db${num}",
+      s"../itest/testdata/localnode${num}.conf"))
+  }
+}
+class UnsafePNodeLauncher {
+
+}
--- tools/src/test/scala/UnsafePNodeLauncherTest.scala
+++ tools/src/test/scala/UnsafePNodeLauncherTest.scala
@@ -1,4 +1,9 @@
+import java.io.{File, FileInputStream}
+import java.util.Properties
+
+import cn.pandadb.network.NodeAddress
 import org.junit.Test
+
 import sys.process._
 
 /**
@@ -11,7 +16,31 @@ class UnsafePNodeLauncherTest {
 
   @Test
   def test1(): Unit = {
-    val startCmd = "cmd.exe /c mvn exec:java -Dexec.mainClass='cn.pandadb.tool.UnsafePNodeLauncher' -Dexec.args=0" !!;
+    val num = 0
+    val startCmd = s"cmd.exe /c mvn exec:java -Dexec.mainClass='cn.pandadb.tool.UnsafePNodeLauncher' -Dexec.args=${num}" !!;
+    val confFile = new File(s"./itest/testdata/localnode${num}.conf")
     Thread.sleep(999999)
+  }
+
+}
+
+import sys.process._
+class PandaDBTestBase {
+  var serialNum = 0
+
+  def startLocalPNodeServer(): NodeAddress = {
+    val startCmd = s"cmd.exe /c mvn exec:java -Dexec.mainClass='cn.pandadb.tool.UnsafePNodeLauncher' -Dexec.args=${serialNum}"
+    startCmd !!;
+    val localNodeAddress = _getLocalNodeAddressFromFile(new File(s"./itest/testdata/localnode${serialNum}.conf"))
+    serialNum += 1;
+    Thread.sleep(100000)
+    localNodeAddress
+  }
+
+  // For base test's use.
+  private def _getLocalNodeAddressFromFile(confFile: File): NodeAddress = {
+    val props = new Properties()
+    props.load(new FileInputStream(confFile))
+    NodeAddress.fromString(props.getProperty("node.server.address"))
   }
 }
