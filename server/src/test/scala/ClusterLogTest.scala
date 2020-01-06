24aa4a67d2569bfeb0731616dd323712888644f9
Airzihao <airzihao@163.com>
2020-01-06 18:54:34 +0800

    Temp restore.

--- server/src/test/scala/ClusterLogTest.scala
+++ server/src/test/scala/ClusterLogTest.scala
@@ -1,7 +1,8 @@
-import java.io.File
+import java.io.{BufferedReader, File, FileInputStream, InputStreamReader}
 
 import cn.pandadb.server.{DataLogDetail, JsonDataLogRW}
 import org.junit.{Assert, BeforeClass, Test}
+import ClusterLogTest.logFile
 /**
   * @Author: Airzihao
   * @Description:
@@ -10,39 +11,47 @@ import org.junit.{Assert, BeforeClass, Test}
   */
 object ClusterLogTest {
 
-  val logFilePath: String = "./datalog.json"
+  val testdataPath: String = "./src/test/testdata/";
+  val logFilePath: String = "./src/test/testdata/datalog.json"
   val logFile = new File(logFilePath)
   @BeforeClass
   def prepareLogFile(): Unit = {
     if (logFile.exists()) {
       logFile.delete()
       logFile.createNewFile()
+    } else {
+      new File(testdataPath).mkdirs()
+      logFile.createNewFile()
     }
   }
 }
 
 class ClusterLogTest {
-  val logFilePath: String = "./datalog.json"
-  val logFile = new File(logFilePath)
 
-  val jsonDataLog = new JsonDataLogRW(logFile)
   val expectedLogArray1: Array[String] = Array("Match(n2), return n2;")
   val expectedLogArray2: Array[String] = Array("Match(n2), return n2;", "Match(n3), return n3;")
 
   @Test
   def test1(): Unit = {
-    jsonDataLog.write(DataLogDetail(100, "Match(n1), return n1;"))
-    jsonDataLog.write(DataLogDetail(200, "Match(n2), return n2;"))
+    val jsonDataLogRW = new JsonDataLogRW(logFile)
+    Assert.assertEquals(0, logFile.length())
+    jsonDataLogRW.write(DataLogDetail(100, "Match(n1), return n1;"))
+    jsonDataLogRW.write(DataLogDetail(200, "Match(n2), return n2;"))
+    val _bf = new BufferedReader(new InputStreamReader(new FileInputStream(logFile)))
+    Assert.assertEquals(s"""{"versionNum":${100},"command":"Match(n1), return n1;"}""", _bf.readLine())
+    Assert.assertEquals(s"""{"versionNum":${200},"command":"Match(n2), return n2;"}""", _bf.readLine())
   }
 
   @Test
   def test2(): Unit = {
-    val commandList = jsonDataLog.consume(logItem => logItem.command, 150)
+    val jsonDataLogRW = new JsonDataLogRW(logFile)
+    val commandList = jsonDataLogRW.consume(logItem => logItem.command, 150)
     Assert.assertEquals(true, expectedLogArray1.sameElements(commandList))
   }
 
   @Test
   def test3(): Unit = {
+    val jsonDataLogRW = new JsonDataLogRW(logFile)
     val jsonDataLog = new JsonDataLogRW(logFile)
     jsonDataLog.write(DataLogDetail(300, "Match(n3), return n3;"))
     val commandList = jsonDataLog.consume(logItem => logItem.command, 150)
