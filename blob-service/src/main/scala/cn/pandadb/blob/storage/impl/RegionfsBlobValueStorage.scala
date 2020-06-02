2ede101ee536a61d29bd2cc53d2a570ecc581602
lzxddz <lzxddz@qq.com>
2020-06-02 14:53:20 +0800

    add RegionfsBlobValueStorage

---
+++ blob-service/src/main/scala/cn/pandadb/blob/storage/impl/RegionfsBlobValueStorage.scala
@@ -0,0 +1,67 @@
+package cn.pandadb.blob.storage.impl
+
+import java.util.UUID
+import java.io.{ByteArrayOutputStream, InputStream}
+import java.nio.ByteBuffer
+
+import cn.pandadb.blob.{Blob, BlobEntry, BlobId, InputStreamSource, MimeType}
+import cn.pandadb.blob.storage.BlobStorageService
+import cn.pandadb.configuration.Config
+import cn.pandadb.util.StreamUtils._
+import org.grapheco.regionfs.client.FsClient
+import org.apache.commons.io.IOUtils
+import org.grapheco.regionfs.FileId
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+class RegionfsBlobValueStorage(config: Config) extends BlobStorageService {
+  val logger = config.getLogger(this.getClass)
+  val fsZKAddress = config.getRegionfsZkAddress()
+
+  val fsClient = new FsClient(fsZKAddress)
+
+  override def save(length: Long, mimeType: MimeType, inputStream: InputStream): BlobEntry = {
+    val baos = new ByteArrayOutputStream()
+    baos.writeLong(length)
+    baos.writeLong(mimeType.code)
+    baos.write(IOUtils.toByteArray(inputStream))
+
+    val future = fsClient.writeFile(ByteBuffer.wrap(baos.toByteArray))
+    val fileId: FileId = Await.result(future, Duration.Inf)
+    val bid = fileId2BlobId(fileId)
+    logger.info(this.getClass + ": save" + "| " + bid.asLiteralString())
+
+    Blob.makeEntry(bid, length, mimeType)
+  }
+
+  override def load(id: BlobId): Option[Blob] = {
+    val future = fsClient.readFile(blobId2FileId(id), ins => {
+      val length = ins.readLong()
+      val mimeType = MimeType.fromCode(ins.readLong())
+      Blob.fromInputStreamSource(id, new InputStreamSource {
+        override def offerStream[T](consume: InputStream => T): T = {
+          val t = consume(ins)
+          ins.close()
+          t
+        }
+      }, length, Some(mimeType))
+    })
+    val blob = Await.result(future, Duration.Inf)
+    logger.info("load blob: " + blob.toString)
+    Some(blob)
+  }
+
+  override def delete(id: BlobId): Unit = {
+    fsClient.deleteFile(blobId2FileId(id)).wait()
+    logger.info("delete blob: " + id.asLiteralString())
+  }
+
+  private def fileId2BlobId(id: FileId): BlobId = {
+    BlobId(id.regionId, id.localId)
+  }
+
+  private def blobId2FileId(id: BlobId): FileId = {
+    FileId(id.value1, id.value2)
+  }
+}
---
+++ blob-service/src/main/test/scala/cn/pandadb/blob/storage/TestsForRegionfsBlobValueStorage.scala
@@ -0,0 +1,26 @@
+package cn.pandadb.blob.storage
+
+import java.io.FileInputStream
+
+import cn.pandadb.blob.MimeType
+import cn.pandadb.blob.storage.impl.RegionfsBlobValueStorage
+import cn.pandadb.configuration.Config
+import org.junit.Test
+
+class TestsForRegionfsBlobValueStorage {
+  val config1 = new Config() {
+    override def getRegionfsZkAddress(): String = {"10.0.82.216:2181"}
+  }
+
+  @Test
+  def test1(): Unit = {
+    val storage = new RegionfsBlobValueStorage(config1)
+    val ins = new FileInputStream("testdata/test.txt")
+    val contentLength = ins.available()
+    val blobEntry = storage.save(10, MimeType.fromCode(1), ins)
+    val blob = storage.load(blobEntry.id).get
+    assert(blob.toString.equals(blobEntry.toString))
+    assert(blob.toBytes().length == contentLength)
+  }
+
+}
