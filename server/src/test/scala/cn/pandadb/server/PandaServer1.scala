eb1797383f3dd578de92bce8a6decae2edea6f37
lzxddz <lzxddz@qq.com>
2020-06-03 18:32:05 +0800

    update config use

--- server/src/test/scala/cn/pandadb/server/PandaServer1.scala
+++ server/src/test/scala/cn/pandadb/server/PandaServer1.scala
@@ -4,11 +4,11 @@ import cn.pandadb.configuration.Config
 
 object PandaServer1 {
   def main(args: Array[String]): Unit = {
-    val config1 = new Config {
-      override def getRpcPort(): Int = 52310
-
-      override def getLocalNeo4jDatabasePath(): String = "output1/db1"
-    }
+    val config1 = new Config()
+    config1.withSettings(Map("rpc.listen.port"->"52310",
+                              "blob.regionfs.zk.address"->"10.0.82.220:2181",
+                              "db.local.data.path" -> "testdata/data1"
+    ))
     val pandaServer1 = new PandaServer(config1)
     pandaServer1.start()
   }
--- server/src/test/scala/cn/pandadb/server/PandaServer2.scala
+++ server/src/test/scala/cn/pandadb/server/PandaServer2.scala
@@ -4,11 +4,12 @@ import cn.pandadb.configuration.Config
 
 object PandaServer2 {
   def main(args: Array[String]): Unit = {
-    val config2 = new Config {
-      override def getRpcPort(): Int = 52320
+    val config2 = new Config()
+    config2.withSettings(Map("rpc.listen.port"->"52320",
+      "blob.regionfs.zk.address"->"10.0.82.220:2181",
+      "db.local.data.path" -> "testdata/data2"
+    ))
 
-      override def getLocalNeo4jDatabasePath(): String = "output2/db2"
-    }
     val pandaServer2 = new PandaServer(config2)
     pandaServer2.start()
   }
